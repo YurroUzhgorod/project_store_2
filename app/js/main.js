f9196a612d719ab2c168743569fb4edd67e41a1d
Yurro-dev <yuriykuliaba@gmail.com>
2024-07-12 16:01:52 +0300

    anded header. started hero container

--- app/js/main.js
+++ app/js/main.js
@@ -1,1 +1,12 @@
+document.addEventListener('DOMContentLoaded', function () {
+	document.onscroll = function () {
+		const header = document.querySelector('.header__top')
+		const scroll = window.scrollY
 
+		if (scroll > 10) {
+			header.classList.add('header-fixed')
+		} else {
+			header.classList.remove('header-fixed')
+		}
+	}
+})
