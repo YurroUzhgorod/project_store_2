db72a187de49c74a14587c8f7fbd9cd025c4bfa2
Yurro-dev <yuriykuliaba@gmail.com>
2024-07-13 23:10:19 +0300

    fixed gulp, added mixitup

--- gulpfile.js
+++ gulpfile.js
@@ -1,24 +1,132 @@
-const { src, dest, watch, parallel, series } = require('gulp')
-const scss = require('gulp-sass')(require('sass'))
-const concat = require('gulp-concat')
-const autoprefixer = require('gulp-autoprefixer')
-const uglify = require('gulp-uglify')
-const imagemin = require('gulp-imagemin')
-const del = require('del')
-const browserSync = require('browser-sync').create()
+// const { src, dest, watch, parallel, series } = require('gulp');
+// const sass = require('gulp-sass')(require('sass'));
+// const concat = require('gulp-concat');
+// const autoprefixer = require('gulp-autoprefixer');
+// const uglify = require('gulp-uglify');
+// const imagemin = require('gulp-imagemin');
+// const del = require('del');
+// const browserSync = require('browser-sync').create();
 
+// // Копіювання mixitup.min.js у папку js
+// function mixitup() {
+// 	return src('./node_modules/mixitup/dist/mixitup.min.js')
+// 		.pipe(dest('app/js'))
+// 		.pipe(browserSync.stream());
+// }
+
+// // Запуск сервера BrowserSync
+// function browsersync() {
+// 	browserSync.init({
+// 		server: {
+// 			baseDir: 'app/',
+// 		},
+// 		notify: false,
+// 	});
+// }
+
+// function styles() {
+// 	return src('app/scss/style.scss')
+// 		.pipe(sass({ outputStyle: 'compressed' }).on('error', sass.logError))
+// 		.pipe(concat('style.min.css'))
+// 		.pipe(
+// 			autoprefixer({
+// 				overrideBrowserslist: ['last 10 versions'],
+// 				grid: true,
+// 			})
+// 		)
+// 		.pipe(dest('app/css'))
+// 		.pipe(browserSync.stream());
+// }
+
+// function scripts() {
+// 	return src(['app/js/main.js', 'app/js/mixitup.min.js'])
+// 		.pipe(concat('main.min.js'))
+// 		.pipe(uglify())
+// 		.pipe(dest('app/js'))
+// 		.pipe(browserSync.stream());
+// }
+
+// function images() {
+// 	return src('./app/images/**/*.*')
+// 		.pipe(
+// 			imagemin([
+// 				imagemin.gifsicle({ interlaced: true }),
+// 				imagemin.mozjpeg({ quality: 75, progressive: true }),
+// 				imagemin.optipng({ optimizationLevel: 5 }),
+// 				imagemin.svgo({
+// 					plugins: [
+// 						{ name: 'removeViewBox', active: true },
+// 						{ name: 'cleanupIDs', active: false },
+// 					],
+// 				}),
+// 			])
+// 		)
+// 		.pipe(dest('dist/images'));
+// }
+
+// // Очистка папки dist
+// function cleanDist() {
+// 	return del('dist');
+// }
+
+// // Збірка проекту
+// function build() {
+// 	return src(['app/**/*.html', 'app/css/style.min.css', 'app/js/main.min.js'], {
+// 		base: 'app',
+// 	}).pipe(dest('dist'));
+// }
+
+// // Спостереження за змінами
+// function watching() {
+// 	watch(['app/scss/**/*.scss'], styles);
+// 	watch(['app/js/**/*.js', '!app/js/main.min.js'], scripts);
+// 	watch(['app/**/*.html']).on('change', browserSync.reload);
+// }
+
+// exports.styles = styles;
+// exports.scripts = series(mixitup, scripts); // Включаємо mixitup() перед scripts()
+// exports.browsersync = browsersync;
+// exports.watching = watching;
+// exports.images = images;
+// exports.cleanDist = cleanDist;
+// exports.build = series(cleanDist, images, build);
+
+// exports.default = parallel(
+// 	styles,
+// 	series(mixitup, scripts),
+// 	browsersync,
+// 	watching
+// );
+
+const { src, dest, watch, parallel, series } = require('gulp');
+const sass = require('gulp-sass')(require('sass'));
+const concat = require('gulp-concat');
+const autoprefixer = require('gulp-autoprefixer');
+const uglify = require('gulp-uglify');
+const imagemin = require('gulp-imagemin');
+const del = require('del');
+const browserSync = require('browser-sync').create();
+
+// Копіювання mixitup.min.js у папку js
+// function mixitup() {
+// 	return src('./node_modules/mixitup/dist/mixitup.min.js')
+// 		.pipe(dest('app/js'))
+// 		.pipe(browserSync.stream());
+// }
+
+// Запуск сервера BrowserSync
 function browsersync() {
 	browserSync.init({
 		server: {
 			baseDir: 'app/',
 		},
 		notify: false,
-	})
+	});
 }
 
 function styles() {
 	return src('app/scss/style.scss')
-		.pipe(scss({ outputStyle: 'compressed' }))
+		.pipe(sass({ outputStyle: 'compressed' }).on('error', sass.logError))
 		.pipe(concat('style.min.css'))
 		.pipe(
 			autoprefixer({
@@ -27,15 +135,15 @@ function styles() {
 			})
 		)
 		.pipe(dest('app/css'))
-		.pipe(browserSync.stream())
+		.pipe(browserSync.stream());
 }
 
 function scripts() {
-	return src('app/js/main.js')
+	return src(['app/js/main.js', './node_modules/mixitup/dist/mixitup.min.js'])
 		.pipe(concat('main.min.js'))
 		.pipe(uglify())
 		.pipe(dest('app/js'))
-		.pipe(browserSync.stream())
+		.pipe(browserSync.stream());
 }
 
 function images() {
@@ -47,43 +155,41 @@ function images() {
 				imagemin.optipng({ optimizationLevel: 5 }),
 				imagemin.svgo({
 					plugins: [
-						{
-							name: 'removeViewBox',
-							active: true,
-						},
-						{
-							name: 'cleanupIDs',
-							active: false,
-						},
+						{ name: 'removeViewBox', active: true },
+						{ name: 'cleanupIDs', active: false },
 					],
 				}),
 			])
 		)
-		.pipe(dest('dist/images'))
+		.pipe(dest('dist/images'));
 }
 
+// Очистка папки dist
+function cleanDist() {
+	return del('dist');
+}
+
+// Збірка проекту
 function build() {
 	return src(['app/**/*.html', 'app/css/style.min.css', 'app/js/main.min.js'], {
 		base: 'app',
-	}).pipe(dest('dist'))
+	}).pipe(dest('dist'));
 }
 
-function cleanDist() {
-	return del('dist')
-}
-
+// Спостереження за змінами
 function watching() {
-	watch(['app/scss/**/*.scss'], styles)
-	watch(['app/js/**/*.js', '!app/js/main.min.js'], scripts)
-	watch(['app/**/*.html']).on('change', browserSync.reload)
+	watch(['app/scss/**/*.scss'], styles);
+	watch(['app/js/**/*.js', '!app/js/main.min.js'], scripts);
+	watch(['app/**/*.html']).on('change', browserSync.reload);
 }
 
-exports.styles = styles
-exports.scripts = scripts
-exports.browsersync = browsersync
-exports.watching = watching
-exports.images = images
-exports.cleanDist = cleanDist
-exports.build = series(cleanDist, images, build)
+exports.styles = styles;
+// exports.scripts = series(mixitup, scripts); // Включаємо mixitup() перед scripts()
+exports.scripts = scripts; // Включаємо mixitup() перед scripts()
+exports.browsersync = browsersync;
+exports.watching = watching;
+exports.images = images;
+exports.cleanDist = cleanDist;
+exports.build = series(cleanDist, images, build);
 
-exports.default = parallel(styles, scripts, browsersync, watching)
+exports.default = parallel(styles, scripts, browsersync, watching);
