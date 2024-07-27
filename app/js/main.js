7f74c4cc653cf5ed4998bf43ba2ef66760cd25be
Yurro-dev <yuriykuliaba@gmail.com>
2024-07-27 21:31:03 +0300

    added reviews section and used swiper.js

--- app/js/main.js
+++ app/js/main.js
@@ -11,4 +11,18 @@ document.addEventListener('DOMContentLoaded', function () {
 	};
 
 	var mixer = mixitup('.pop-food__content');
+
+	const swiper = new Swiper('.swiper', {
+		loop: true,
+
+		pagination: {
+			el: '.swiper-pagin',
+			clickable: true,
+		},
+
+		navigation: {
+			nextEl: '.swiper-btn--prev',
+			prevEl: '.swiper-btn--next',
+		},
+	});
 });
